e1a64bf1af6b198368ff4aadccf518d339986164
Zhao Owen <kikakkz@hotmail.com>
2018-07-11 00:06:46 +0800

    Remove old compile script

--- project/source.mk
+++
@@ -1,8 +0,0 @@
-COMMON_INCLUDE_DIRS += $(rootdir)/$(MODULE)/include         \
-                       $(incdir)/libonplatform
-
-COMMON_SRC_FILES := $(rootdir)/$(MODULE)/src/onevent.c     \
-                    $(rootdir)/$(MODULE)/src/onevgrp.c     \
-                    $(rootdir)/$(MODULE)/src/onlfds.c
-
-COMMON_INST_HEADER_DIRS += $(rootdir)/$(MODULE)/include
